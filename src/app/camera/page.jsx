868e203c473fe9f5506942db675e7e584baa9ad2
demonic29 <khaleesikomodo@gmail.com>
2024-10-28 11:55:05 +0900

    intro screen

---
+++ src/app/camera/page.jsx
@@ -0,0 +1,41 @@
+'use client';
+import React, { useRef, useEffect, useState } from "react";
+import { Camera } from "react-camera-pro";
+
+const CameraScreen = () => {
+    const camera = useRef();
+    // const [isClient, setIsClient] = useState(false);
+
+    // useEffect(() => {
+    //     setIsClient(true); // Ensures the component renders only on the client
+    // }, []);
+
+    return (
+        <div className="h-screen">
+            {/* {isClient && (
+                <Camera
+                    ref={camera}
+                    errorMessages={{
+                        noCameraAccessible: "",
+                        permissionDenied: "",
+                        switchCamera: "",
+                        canvas: "",
+                    }}
+                    className="h-screen w-screen"
+                />
+            )} */}
+            <Camera
+                    ref={camera}
+                    errorMessages={{
+                        noCameraAccessible: "",
+                        permissionDenied: "",
+                        switchCamera: "",
+                        canvas: "",
+                    }}
+                    className="h-screen w-screen bg-cyan-100"
+                />
+        </div>
+    );
+};
+
+export default CameraScreen;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"], // This should include all HTML files in `src` and subfolders
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
   plugins: [],
-}
+};
