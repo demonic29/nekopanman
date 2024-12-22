9c55423f4a302dedbffc182c4569bd7d1e19941e
demonic29 <khaleesikomodo@gmail.com>
2024-12-22 21:11:40 +0900

    push

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
-import {nextui} from '@nextui-org/react';
+import { nextui } from '@nextui-org/react';
 
-
-// const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     // "./node_modules/flowbite/**/*.js"
-
   ],
   theme: {
     extend: {
@@ -23,17 +20,18 @@ module.exports = {
   plugins: [
     nextui(),
     // flowbite,
-
-    function({addUtilities}){
+    function ({ addUtilities }) {
       addUtilities({
-        '.hidden-scrollbar' : {
+        '.hidden-scrollbar': {
           'scrollbar-width': 'none',
           'ms-overflow-style': 'none',
         },
-        '.hidden-scrollbar::-webkit-scrollbar':{
-          display:'none'
-        }
-      })
-    }
+        '.hidden-scrollbar::-webkit-scrollbar': {
+          display: 'none',
+        },
+      });
+    },
   ],
 };
+
+export default config;
